709f56cad3aaf9259e2e4a0f4c3afd2408253b76
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-03-21 16:32:35 +0100

    Test endpoints

--- go.mod
+++ go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/stretchr/testify v1.3.0
 	github.com/ugorji/go/codec v0.0.0-20190316192920-e2bddce071ad // indirect
 	golang.org/x/net v0.0.0-20190313220215-9f648a60d977 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
---
+++ main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRootEndpoint(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestNonExistentEndpoint(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/blockchain", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 404, w.Code)
+}
